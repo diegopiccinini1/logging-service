624c96b8a783faac56f769245e2e6a1fcf05d0e4
Diego Piccinini <diego.piccinini.studio@fitstic-edu.com>
2021-03-23 11:54:57 +0100

    test, ci.yaml

---
+++ tests/app.test.js
@@ -0,0 +1,11 @@
+const request = require("supertest");
+const app = require("../src/app");
+
+describe("test app rest endpoints",()=>{
+    test("It should return hello world", async() =>{
+        const response = await request(app).get("/");
+
+        expect(response.statusCode).toBe(200)
+        expect(response.body).toEqual("Hello World!!!")
+        })
+})
